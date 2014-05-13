d72d5f959f8414d329df5f58200e6ed508082821
Swapnil Patil <swapnil.patil@weboniselab.com>
2014-05-13 19:23:21 +0530

    added cofigurations

--- protected/config/main.php
+++ protected/config/main.php
@@ -6,12 +6,6 @@
 // This is the main Web application configuration. Any writable
 // CWebApplication properties can be configured here.
 
-define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
-define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
-define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
-define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
-define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
-
 return array(
 	'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
 	'name'=>'My Web Application',
--- protected/yiic.php
+++ protected/yiic.php
@@ -1,6 +1,12 @@
 <?php
 
 // change the following paths if necessary
+define('DB_HOST', getenv('OPENSHIFT_MYSQL_DB_HOST'));
+define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
+define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
+define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
+define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+
 $yiic=dirname(__FILE__).'/../framework/yiic.php';
 $config=dirname(__FILE__).'/config/console.php';
 
