4e90870c529026ba5353cd4f3fbd141e10a7f919
Swapnil Patil <swapnil.patil@weboniselab.com>
2014-05-14 13:19:39 +0530

    echo

--- index.php
+++ index.php
@@ -5,6 +5,7 @@ define('DB_PORT',getenv('OPENSHIFT_MYSQL_DB_PORT'));
 define('DB_USER',getenv('OPENSHIFT_MYSQL_DB_USERNAME'));
 define('DB_PASS',getenv('OPENSHIFT_MYSQL_DB_PASSWORD'));
 define('DB_NAME',getenv('OPENSHIFT_GEAR_NAME'));
+echo getenv('OPENSHIFT_MYSQL_DB_HOST');
 // include Yii bootstrap file
 require_once(dirname(__FILE__).'/framework/yii.php');
 $config=dirname(__FILE__).'/protected/config/main.php';
